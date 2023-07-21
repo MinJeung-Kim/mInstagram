20ef9fcd3ef3f759b327010a2276b3d1316e4e1d
MinJeung-Kim <focso5@gmail.com>
2023-07-21 18:35:52 +0900

    Add: Use react-multi-carousel Library

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -4,33 +4,45 @@ import Link from "next/link";
 import { PropagateLoader } from "react-spinners";
 import useSWR from "swr";
 import Avatar from "./Avatar";
+import ScrollableBar from "./ui/ScrollableBar";
+
 export default function FollowingBar() {
   // 1. client component에서 백엔드에게 api/me 사용자의 정보를 얻어옴.
   // 2. 백엔드에서는 현재 로그인된 사용자의 세션 정보를 이용해서
   // 3. 백엔드에서 사용자의 상세 정보를 Sanity에서 가지고 옴 (followings)
   const { data, isLoading: loading, error } = useSWR<DetailUser>("/api/me");
-  const users = data?.following; 
-  
+  // const users = data?.following;
+  // const users = undefined;
+  const users = data?.following && [
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+  ];
+
   // 4. 여기에서, 클라이언트 컴포넌트에서 followings의 정보를 UI에 보여줌.
   // (image, username)
   return (
-    <section>
+    <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
       {loading ? (
         <PropagateLoader size={8} color="red" />
       ) : (
         (!users || users.length === 0) && <p>{`You don't have following`}</p>
       )}
       {users && users.length > 0 && (
-        <ul>
+        <ScrollableBar>
           {users.map(({ image, username }) => (
-            <li key={username}>
-              <Link href={`/user/${username}`}>
-                <Avatar image={image} highlight />
-                <p>{username}</p>
-              </Link>
-            </li>
+            <Link
+              key={username}
+              className="flex flex-col items-center w-20"
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} highlight />
+              <p className="w-full text-sm text-center text-ellipsis overflow-hidden">
+                {username}
+              </p>
+            </Link>
           ))}
-        </ul>
+        </ScrollableBar>
       )}
     </section>
   );
