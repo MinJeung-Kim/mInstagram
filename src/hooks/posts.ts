25b8ef17c983fa4c6c0a14299276341c75c2be7d
MinJeung-Kim <focso5@gmail.com>
2023-07-28 12:59:45 +0900

    Add: 즉각적인 ui update - Bound Mutate

--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -1,20 +1,48 @@
 import { SimplePost } from "@/model/post";
-import useSWR, { useSWRConfig } from "swr";
+import useSWR from "swr";
 
-// posts를 가지고 오기 위해서 어떤 url 어떤 캐시키를 사용하는지,
-// 좋아요가 변경되면 내부에서 어떤일을 해주어야 하는지 캡슐화 해주는 hook
+async function updateLike(id: string, like: boolean) {
+  return fetch("/api/likes", {
+    method: "PUT",
+    body: JSON.stringify({ id, like }),
+  }).then((res) => res.json());
+}
+
 export default function usePosts() {
-  // posts를 가지고 오는 로직.
-  const { data: posts, isLoading, error } = useSWR<SimplePost[]>("/api/posts");
-  const { mutate } = useSWRConfig();
+  const {
+    data: posts,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR<SimplePost[]>("/api/posts");
 
-  // 어떤 post에 어떤 사용자의 username을 like할건지 ?
+  // local상으로 update할 변경될 posts 배열 생성.
   const setLike = (post: SimplePost, username: string, like: boolean) => {
-    // 
-    fetch("/api/likes", {
-      method: "PUT",
-      body: JSON.stringify({ id: post.id, like }),
-    }).then(() => mutate("/api/posts"));
+    // 기존 post를 그대로 가지고 가면서 likes배열을 업데이트해줌.
+    // like = true : 기존 likes를 그대로 가져가면서 username를 추가.
+    // like = false : likes배열에서 username를 제거.
+    const newPost = {
+      ...post,
+      likes: like
+        ? [...post.likes, username]
+        : post.likes.filter((item) => item !== username),
+    };
+
+    // "/api/posts"를 업데이트해주기 위한 배열
+    // 배열안의 post(p.id)를 업데이트하고자하는 post(post.id)와 같으면 업데이트
+    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+
+    // 바운드된 mutate함수 호출 
+    // param 1 => updateLike(post.id, like) : 반환된 값으로 api post 데이터를 덮음.
+    return mutate(updateLike(post.id, like), {
+      optimisticData: newPosts,
+      // populateCache: false: param 1로 반환된 값으로 덮지않겠다.
+      populateCache: false,
+      // updateLike가 성공하면 server에서 다시 데이터를 가져오지 않겠다.
+      revalidate: false,
+      // 네트워크 문제가 발생해서 server에 제대로 업데이트가 되지않았다면rollback하겠다. 
+      rollbackOnError: true,
+    });
   };
 
   return { posts, isLoading, error, setLike };
