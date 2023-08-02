6c0cb138d1267fb9033977d6aedbf5d333c9dd29
MinJeung-Kim <focso5@gmail.com>
2023-08-02 11:42:08 +0900

    Add: follow handler - transaction

--- src/service/user.ts
+++ src/service/user.ts
@@ -1,5 +1,6 @@
-import { SearchUser } from '@/model/user';
-import { client } from './sanity';
+import { SearchUser } from "@/model/user";
+import { client } from "./sanity";
+import { idText } from "typescript";
 
 type OAuthUser = {
   id: string;
@@ -12,7 +13,7 @@ type OAuthUser = {
 export async function addUser({ id, username, email, name, image }: OAuthUser) {
   return client.createIfNotExists({
     _id: id,
-    _type: 'user',
+    _type: "user",
     username,
     email,
     name,
@@ -38,7 +39,7 @@ export async function getUserByUsername(username: string) {
 export async function searchUsers(keyword?: string) {
   const query = keyword
     ? `&& (name match "${keyword}") || (username match "${keyword}")`
-    : '';
+    : "";
   return client
     .fetch(
       `*[_type =="user" ${query}]{
@@ -81,10 +82,10 @@ export async function addBookmark(userId: string, postId: string) {
   return client
     .patch(userId) //
     .setIfMissing({ bookmarks: [] })
-    .append('bookmarks', [
+    .append("bookmarks", [
       {
         _ref: postId,
-        _type: 'reference',
+        _type: "reference",
       },
     ])
     .commit({ autoGenerateArrayKeys: true });
@@ -95,4 +96,33 @@ export async function removeBookmark(userId: string, postId: string) {
     .patch(userId)
     .unset([`bookmarks[_ref=="${postId}"]`])
     .commit();
+}
+
+// 나의 사용자 id , target사용자 id를 받음
+export async function follow(myId: string, targetId: string) {
+  return (
+    client
+      .transaction()
+      // 나의 id에대해 patch => 내 사용자 데이터가 콜백으로 전달됨.
+      .patch(myId, (user) =>
+        user
+          // following이 비어있다면 빈배열로 set.
+          .setIfMissing({ following: [] })
+          // following키의 배열에 데이터 추가. 데이터는 객체로 전달.
+          .append("following", [{ _ref: targetId, _type: "reference" }])
+      )
+      .patch(targetId, (user) =>
+        user
+          .setIfMissing({ followers: [] })
+          .append("followers", [{ _ref: myId, _type: "reference" }])
+      )
+      .commit({ autoGenerateArrayKeys: true })
+  );
+}
+export async function unfollow(myId: string, targetId: string) {
+  return client
+    .transaction()
+    .patch(myId, (user) => user.unset([`following[_ref=="${targetId}"]`]))
+    .patch(targetId, (user) => user.unset([`followers[_ref=="${myId}"]`]))
+    .commit({ autoGenerateArrayKeys: true });
 }
